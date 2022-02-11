479a147f022716f099a5576052330f438e6be865
HM1 <mrtoscano94@gmail.com>
2022-02-11 18:04:14 -0500

    React Master 1er

---
+++ src/homepage.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import './homepage.styles.scss'
+
+const Hompage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='menu-item'>Hats</h1>
+                    <span className='menu-item'>Hats</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='menu-item'>Sneackers</h1>
+                    <span className='menu-item'>Hats</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='menu-item'>Jackets</h1>
+                    <span className='menu-item'>Hats</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='menu-item'>WOMER</h1>
+                    <span className='menu-item'>Hats</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='menu-item'>MEN</h1>
+                    <span className='menu-item'>Hats</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+// ! Don't forget to export me. 
+export default Hompage
