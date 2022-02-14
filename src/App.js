51824fb9956fbe37d4d20a3deaf45143cf12296d
HM1 <mrtoscano94@gmail.com>
2022-02-14 12:16:10 -0500

    Updated the menu items to anvigate with router HOC

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React from 'react';
 
 //  * Componentes
-
 import Hompage from './pages/homepage/homepage.component';
 
+//  * Router 
+import { Switch, Route } from 'react-router-dom'
+
+const HatsPage = () => (
+  <div>
+    <h1>Gorras Component</h1>
+  </div>
+);
+
+/*
+? Router Notes
+* Route is a component that mainly takes 3 props
+! In case we decide not tu use exact, router will show all the routes that partially matched
+* "exact"     : boolean =  If true, the path need to be exact for the router to show 
+* "path"      : string  =  Is the path Where this component will be shown
+* "component" : string  =  Is the compone
+*/
+
+// * In case you decide to use <Switch><Switch/> 
+// ! Only one component will be shown as soon as it matches with. 
 function App() {
   return (
-    <div className="App">
-      <Hompage />
-    </div>
+    <Switch>
+      <div className="App">
+        <Route exact path='/' component={Hompage} />
+        <Route exact path='/hats' component={HatsPage} />
+      </div>
+    </Switch>
   );
 }
 
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-
+//  * Components
+import { withRouter } from 'react-router-dom'
+//  * Syles
 import './menu-item.styles.scss'
 
 //  * Instead of passing only pros to the component
 //  * When we get the parameters we destructure them
 //  * Avoiding using props.title or props.subMenu
-const MenuItem = ({ title, imageUrl, size }) => (
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
     //  * To add dynamic styles in componentes,
     //  * We can do that by passing an object inside
     //  * style and interpolate the dynamic value
-    <div className={`${size} menu-item`}>
-
+    <div
+        className={`${size} menu-item`}
+        onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div className='background-image'
             style={{
                 backgroundImage: `url(${imageUrl})`,
@@ -21,10 +24,13 @@ const MenuItem = ({ title, imageUrl, size }) => (
             <span className='menu-item'>Hats</span>
         </div>
 
-    </div>
+    </div >
 )
 
-export default MenuItem
+export default withRouter(MenuItem)
 
 
-
+// ! WithRouters takes a components a s a parameter and return a component
+// * After passing our components to withRouter
+// * now we can access the props we need instead of DRILLING PROPS
+// * LOCATION MATCH AND HISTORY 
