20fef604709062d0749497f2de32866800824df1
HM1 <mrtoscano94@gmail.com>
2022-02-16 18:13:46 -0500

    Cart item component created and conected with REDUX store

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
-
+//  * Components
 import CustomButton from '../custom-button/custom-button.component'
-
+import CartItem from '../cart-item/cart-item.component'
+// * Styles
 import './cart-dropdown.styles.scss'
+//  * Redux
+import { connect } from 'react-redux'
 
-
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
     <div className='cart-dropdown'>
         <div className='cart-items' />
+        {
+            cartItems.map(cartItem => (<CartItem key={cartItem.id} item={cartItem} />))
+        }
         <CustomButton>Go to checkout</CustomButton>
     </div>
 )
 
-export default CartDropdown 
+// ? This function allow us to receive 
+// ? redux store as a parameter and, then use it as props.
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems
+})
+
+export default connect(mapStateToProps)(CartDropdown)  
