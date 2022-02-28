365ad772a725f5a86b20cc90c49e695a65346f62
HM1 <mrtoscano94@gmail.com>
2022-02-28 18:42:16 -0500

    Removed logger from production env

--- src/redux/store.js
+++ src/redux/store.js
@@ -10,7 +10,14 @@ import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 //  ? We define it like this in case 
 //  ? we want to pass more than one middleware  
-const middlewares = [logger];
+const middlewares = [];
+
+//  ? Asi podemos mostrar logger en caso de que nos encontremos en el dev envronment 
+
+if (process.env.NODE_ENV === "development") {
+    middlewares.push(logger)
+}
+
 // ? Create the redux store
 // ? first parameter is our rootReducer
 // ? second is how we apply middlewares
